f202e72ad45eb9f6a2808d3268be666ef0b20947
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-01 11:33:18 -0600

    commit tree ce57b7add02f43adefe82cf4355302d3ebecdb78

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result 
+  if (distance <= 400) {
+    return `This one is on me!`;
+  }
+   else if (distance <= 2000) {
+    return `That will be twenty bucks.`;
+  }
+    else if (distance <= 2500) {
+    return `I will gladly take your thirty bucks.`
+   }
+   else {
+    return `No can do.`;
+   }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === `NYC`) {
+  return `Ok, sounds good.`
+  }
+  else {
+    return `No go.`
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === `generous`) {
+    return `Thank you so much.`
+  }
+  else if (tip === `not as generous`) {
+    return `Thank you.`
+  }
+  else {
+    return `Bye.`
+  }
 }
+
